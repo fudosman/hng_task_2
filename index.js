41972ff2043546fbc50da1c0eaa217bb785b9a9a
fudosman <fudosman@gmail.com>
2022-11-02 20:14:05 +0100

    testing my gitignore

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const request=require('request')
-const csv=require('csvtojson')
- 
+const request = require("request");
+const csv = require("csvtojson");
+
 csv()
-.fromStream(request.get('https://docs.google.com/spreadsheets/d/1-9zf50iUmdtvpbEvQ7I-M2vlK8hCSB1DC_bF5bDNjxE/edit#gid=663257143'))
-.subscribe((json)=>{
-    return new Promise((resolve,reject)=>{
-        // long operation for each json e.g. transform / write into database.
-        console.log(json)
-    })
-});
+  .fromStream(
+    request.get(
+      "https://docs.google.com/spreadsheets/d/1-9zf50iUmdtvpbEvQ7I-M2vlK8hCSB1DC_bF5bDNjxE/edit#gid=663257143"
+    )
+  )
+  .subscribe((json) => {
+    return new Promise((resolve, reject) => {
+      // long operation for each json e.g. transform / write into database.
+      console.log(json);
+    });
+  });
+
+// test
