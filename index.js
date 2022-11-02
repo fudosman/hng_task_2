718a0294aa9f6b44c976fa224fc9beed19cf7eda
fudosman <fudosman@gmail.com>
2022-11-03 00:27:56 +0100

    done with the task

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const request = require("request");
 const csv = require("csvtojson");
+const { json } = require("stream/consumers");
+const sha256 = require("js-sha256").sha256;
 
-csv()
-  .fromStream(
-    request.get(
-      "https://docs.google.com/spreadsheets/d/1-9zf50iUmdtvpbEvQ7I-M2vlK8hCSB1DC_bF5bDNjxE/edit#gid=663257143"
-    )
-  )
-  .subscribe((json) => {
-    return new Promise((resolve, reject) => {
-      // long operation for each json e.g. transform / write into database.
-      console.log(json);
-    });
+const csvFilePath =
+  "/home/assocmaster/Desktop/CODES/HNG/Task_2/csv_files/NFT Naming csv - Team Clutch.csv";
+
+async function convertFile(csvFilePath) {
+  const jsonArray = await csv().fromFile(csvFilePath);
+  // console.log(jsonArray);
+  jsonArray.forEach((record) => {
+    const sha256_for_each_row = sha256(record.UUID);
+    record[`${record.Filename}.sha256.csv`] = sha256_for_each_row;
+    console.log(record);
   });
+  console.log(jsonArray);
+}
 
-// test
+convertFile(csvFilePath);
