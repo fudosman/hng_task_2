79f18fca08ed236026b43232cb9b11fddf81fc50
fudosman <fudosman@gmail.com>
2022-11-05 12:11:44 +0100

    done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const csv = require("csvtojson");
 const sha256 = require("js-sha256").sha256;
+const jsonToCSV = require('json-to-csv');
 
 const csvFilePath =
   "/home/assocmaster/Desktop/CODES/HNG/Task_2/csv_files/NFT Naming csv - Team Clutch.csv";
@@ -11,13 +12,19 @@ async function convertFile(csvFilePath) {
 
   jsonArray.forEach((record) => {
     // hash each record
-    const hashKey = "mighty hash";
-    const sha256_for_each_row = sha256(record + hashKey);
+    console.log(record)
+    const sha256_for_each_row = sha256(record.UUID);
 
     // append the hash to each record
-    record[`${record.Filename}.sha256.csv`] = sha256_for_each_row;
+    record[`record.Filename}.sha256.csv`] = sha256_for_each_row;
   });
   console.log(jsonArray);
+  const fileName = csvFilePath.split("/")[8].split(".")[0];
+  const newCSV = `${fileName}.sha256.csv`;
+  // console.log(newCSV)
+  const new_csv_file = jsonToCSV(jsonArray,newCSV);
+  // console.log(new_csv_file);
+
   return jsonArray;
 }
 
